578ec294427a5972afdac1e6296cd54f287181e0
Aleksandrs Berdicevskis <alexberd@gmail.com>
2020-10-13 12:13:28 +0200

    Scripts and data updated

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # subject-encoding
-variable names (go through the file in details
+This repository contains the data and the scripts that are necessary to reproduce the results reported in:
+
+Berdicevskis, Aleksandrs, Karsten Schmidtke-Bode and Ilja Seržant. 2020. Subjects tend to be coded only once: Corpus-based and grammar-based evidence for an efficiency-driven trade-off. In: Proceedings of the 19th International Workshop on Treebanks and Linguistic Theories (TLT).
+
+The repository contains the following files and folders:
+- corpus_analysis.rb: a script that goes through the UD corpora (train, dev and test concatenated). If a language has several UD corpora, they have to be concatenated into a single on (use  ud_merge_perlang.rb)
+
+- subj_data.csv is a tab-separated file which contains the data analyzed in Section 3 of the article (it is the output of corpus_analysis.rb and the input for data_analysis.r). It has the following columns:
+-- lemma (verb lemma);
+-- sentence (full sentence where the verb occurs; note that our level of analysis is the clause, sentences are provided just as wide context);
+-- tense (tense-mood combination which has two values with somewhat imprecise names: Past (=l-participles) and Nonpast (all other forms);
+-- aspect (Imp = imperfective or Perf = perfective);
+-- sperson (subject person as can be inferred from the verbal form. Possible values: 3, 12 (=1 or 2), unknown);
+-- expressed (1 or 0: whether there is a free syntactic subject of any kind (either pronoun or full NP));
+-- pronexpressed (1 or 0: whether there is a free syntactic subject encoded by a personal pronoun);
+-- pron12expressed ((1 or 0: whether there is a free syntactic subject encoded by a first- or second-person pronoun);
+-- lang (language ISO code);
+-- group (language group: non_copular (= East Slavic) or copular (= South and West Slavic));
+-- rel (incoming syntactic relation for the verb);
+-- clause_type (as inferred from the UD annotation: simple, main, subord(inate) or coord(inate)).
+
+- ud_merge_perlang.rb: a script that goes through all UD 2.6 treebanks and merges all treebanks for the same language into a single file. Set the IN and OUT paths at the beginning of the script
+
+- data_analysis.r: an R script that performs the statistical analysis described in Section 3 and creates the figures. Make sure the necessary packages are installed, change the R directory to the folder with subj_data.csv
+
 additional model
-merge_perlang
-corpus_analysis (comment)
-data_analysis (comment)
-data
+
 
 Karsten
--- corpus_analysis.rb
+++ corpus_analysis.rb
@@ -1,7 +1,9 @@
-#require 'translit'
+STDERR.puts "Usage: ruby corpus_analysis.rb PATH-TO-CORPORA"
 
+#Path to the UD corpora
 PATH = ARGV[0]
 
+#thresholds (as described in the article)
 THRESHOLD_SUBJ = 0
 THRESHOLD_TOTAL = 0
 
@@ -9,16 +11,20 @@ PRONOUNS = {"rus"=>["я", "ты", "мы", "вы", "он", "она", "оно", "�
 PRONOUNS12 = {"rus"=>["я", "ты", "мы", "вы"], "ukr" => ["я", "ти", "ми", "ви"],"pol"=>["ja","ty","my","wy"], "cze"=>["já","ty","my","vy"],"slk"=>["ja","ty","my","vy"], "srb"=>["ja","ti","mi","vi"],"crt"=>["ja","ti","mi","vi"],"blg"=>["аз", "ти", "ние", "вие"],"slv"=>["jaz","ti","mi","vi"], "bel"=>["я", "ты", "мы", "вы"], "hsb" => ["ja", "ty", "my", "wy"]}
 #in Slovenian treebank mi and vi are actually forms of ja and ti, the same is true for Upper Sorbian (and for dual in both languages). Not a problem for us.
 
+#hash used to label clause types
 RELHASH = {"conj" => "coord", "parataxis" => "coord", "csubj" => "subord", "ccomp" => "subord", "xcomp" => "subord", "advcl" => "subord", "acl" => "subord", "root" => "main"}
+
+#if set to true then all conjuncts of the verb that has a free syntactic subject will be assumed to have it (i.e. "drink" in "I eat and drink" will be coded as having a pronominal subject).
 CHECK_COORD = false
 
+#is there a subject among the children of the verb
 def check_for_subj_child(hash, sentence, lang)
     nosubject = true
     pronsubject = false
     pron12subject = false
     if !hash["children"].nil?
         hash["children"].each do |child|
-        if sentence[child]["rel"].include?("subj") #both csubj and nsubj! both pass and non-pass
+        if sentence[child]["rel"].include?("nsubj") #both pass and non-pass
         #expressed = 1
         nosubject = false
         if PRONOUNS[lang].include?(sentence[child]["lemma"])
@@ -35,6 +41,7 @@ def check_for_subj_child(hash, sentence, lang)
     return [nosubject,pronsubject,pron12subject]
 end
 
+#method for telling apart main and subordinate clauses
 def check_for_subord_child(hash, sentence)
     subord = false
     if !hash["children"].nil?
@@ -53,21 +60,20 @@ end
 files = {"bel" => "Belarusian.conllu", "rus"=>"Russian.conllu", "ukr" => "Ukrainian.conllu", "pol" => "Polish.conllu", 
 "cze" => "Czech.conllu", "slk" => "Slovak.conllu", "blg" => "Bulgarian.conllu", "slv" => "Slovenian.conllu","crt"=>"Croatian.conllu","srb"=>"Serbian.conllu", "test" => "test.conllu", "ocs" => "Old_Church_Slavonic.conllu", "orv" => "Old_Russian.conllu", "hsb" => "Upper_Sorbian.conllu"}
 
-
-detfile = File.open("subj_data.csv", "w:utf-8")
+#output file
+detfile = File.open("subj_data2.csv", "w:utf-8")
 detfile.puts "lemma\tsentence\ttense\taspect\tsperson\texpressed\tpronexpressed\tpron12expressed\tlang\tgroup\trel\tclause_type"
+
+#languages that will be processed
 langs = ["bel", "rus", "ukr", "pol", "cze", "slk", "blg", "slv", "crt", "srb", "hsb"]
 
-
+#hashes to infer correct tense-mood labelling of verbs from the UD annotation
 pasttenses = Hash.new(["Past"]) #rus, ukr, pol, czech, slovak, bel
-#pasttenses["blg"] = ["Past","Imp"]
-#pasttenses["crt"] = ["Past","Imp"]
 pasttenses["slv"] = ["unknown"]
-#pasttenses["srb"] = ["unknown"]
-#pasttenses["blg"] = ["unknown"]
 personhash = {"1"=>"12","2"=>"12","3"=>"3","unknown"=>"unknown"}
 
-def findfeat(feats,feat) #finds the value of a given feat
+#finds the value of a given feat
+def findfeat(feats,feat) 
     value = "unknown"
     feats2 = feats.split("|")
     feats2.each do |feat_value|
@@ -96,140 +102,110 @@ langs.each do |lang|
     sentences = []
     sentence = Hash.new{|hash, key| hash[key] = Hash.new;} 
     verbs = Hash.new{|hash, key| hash[key] = Hash.new(0);} 
-    verbs_output = Hash.new{|hash, key| hash[key] = Array.new;} 
+    verbs_output = Hash.new{|hash, key| hash[key] = Array.new;}
     text = ""
     
     f.each_line do |line|
         line1 = line.strip
         #STDERR.puts line1
-        if line1[0..5]=="# text"
+        if line1[0..5]=="# text" #running text 
             text = line1.split("=")[1][1..-1].gsub("\"","'")
-        elsif line1 != "" and line1[0] != "#"
+        elsif line1 != "" and line1[0] != "#" #tokens
             line2 = line1.split("\t") 
-        
-        sentence[line2[0]]["form"] = line2[1] #form
-        sentence[line2[0]]["lemma"] = line2[2] #lemma
-        sentence[line2[0]]["pos"] = line2[3] #pos
-        
-        sentence[line2[0]]["feats"] = line2[5] #feats
-        sentence[line2[0]]["head"] = line2[6] #head
-        sentence[line2[0]]["rel"] = line2[7] #rel
-        if sentence[line2[6]]["children"].nil? #skip for 0?
-            sentence[line2[6]]["children"] = [line2[0]]
-        else 
-            sentence[line2[6]]["children"] << line2[0] #head: children
-        end    
-        
-        elsif line1 == ""        
+            sentence[line2[0]]["form"] = line2[1] #form
+            sentence[line2[0]]["lemma"] = line2[2] #lemma
+            sentence[line2[0]]["pos"] = line2[3] #pos
+            sentence[line2[0]]["feats"] = line2[5] #feats
+            sentence[line2[0]]["head"] = line2[6] #head
+            sentence[line2[0]]["rel"] = line2[7] #rel
+            if sentence[line2[6]]["children"].nil? 
+                sentence[line2[6]]["children"] = [line2[0]]
+            else 
+                sentence[line2[6]]["children"] << line2[0] #head: children
+            end    
+        elsif line1 == "" #end of sentence
             sentences << sentence
-            sentence.each_pair do |node, hash| 
-            if hash["pos"] == "VERB" and ( (["test","rus","ukr","bel"].include?(lang) and findfeat(hash["feats"],"VerbForm")=="Fin" and findfeat(hash["feats"],"Tense")!="unknown") or (["pol","cze","slk"].include?(lang) and findfeat(hash["feats"],"Tense")!="unknown") or (["srb","slv"].include?(lang) and ((findfeat(hash["feats"],"Gender")!="unknown" and findfeat(hash["feats"],"Number")!="unknown" and findfeat(hash["feats"],"Case")=="unknown") or (findfeat(hash["feats"],"Tense")!="unknown"))) or (["crt","blg"].include?(lang) and ((findfeat(hash["feats"],"VerbForm")=="Part" and findfeat(hash["feats"],"Tense")=="Past") or (findfeat(hash["feats"],"VerbForm")=="Fin" and findfeat(hash["feats"],"Tense")!="Unknown")) ) ) #and !["csubj", "ccomp", "xcomp", "advcl", "acl"].include?(hash["rel"])
-            if verbs[hash["lemma"]]["Past"] == 0 
-                verbs[hash["lemma"]]["Past"] = Hash.new(0)
-            end
-            if verbs[hash["lemma"]]["Nonpast"] == 0 
-                verbs[hash["lemma"]]["Nonpast"] = Hash.new(0)
-            end
-            verbs[hash["lemma"]]["freq"] += 1 #total frequency
-            
-            
-            if pasttense.include?(findfeat(hash["feats"],"Tense")) and (lang!="blg" or (lang=="blg" and findfeat(hash["feats"],"VerbForm")=="Part"))
-                tense = "Past"
-            else
-                tense = "Nonpast"
-            end
-            
-            verbs[hash["lemma"]][tense]["freq"]+=1
-            
-            subjectinfo = check_for_subj_child(hash, sentence, lang)
-            nosubject = subjectinfo[0]
-            pronsubject = subjectinfo[1]
-            pron12subject = subjectinfo[2]
-            if CHECK_COORD
-                if nosubject and hash["rel"] == "conj" 
-                    hash2 = sentence[hash["head"]] #we are going up, to the first conjunct. In principle other conjuncts can be checked as well
+            sentence.each_pair do |node, hash|
+                if hash["pos"] == "VERB" and ( (["test","rus","ukr","bel"].include?(lang) and findfeat(hash["feats"],"VerbForm")=="Fin" and findfeat(hash["feats"],"Tense")!="unknown") or (["pol","cze","slk"].include?(lang) and findfeat(hash["feats"],"Tense")!="unknown") or (["srb","slv"].include?(lang) and ((findfeat(hash["feats"],"Gender")!="unknown" and findfeat(hash["feats"],"Number")!="unknown" and findfeat(hash["feats"],"Case")=="unknown") or (findfeat(hash["feats"],"Tense")!="unknown"))) or (["crt","blg"].include?(lang) and ((findfeat(hash["feats"],"VerbForm")=="Part" and findfeat(hash["feats"],"Tense")=="Past") or (findfeat(hash["feats"],"VerbForm")=="Fin" and findfeat(hash["feats"],"Tense")!="Unknown")) ) ) #if this a relevant verb form (and not, for instance, an infinitive or a participle other than l-participle). The conditions may have to be changed with new versions of UD (later than 2.6)
                     
-                    subjectinfo = check_for_subj_child(hash2, sentence, lang)
+                    #storing info about the verb and its tense
+                    if verbs[hash["lemma"]]["Past"] == 0 
+                        verbs[hash["lemma"]]["Past"] = Hash.new(0)
+                    end
+                    if verbs[hash["lemma"]]["Nonpast"] == 0 
+                        verbs[hash["lemma"]]["Nonpast"] = Hash.new(0)
+                    end
+                    verbs[hash["lemma"]]["freq"] += 1 #total frequency
+                    if pasttense.include?(findfeat(hash["feats"],"Tense")) and (lang!="blg" or (lang=="blg" and findfeat(hash["feats"],"VerbForm")=="Part"))
+                        tense = "Past"
+                    else
+                        tense = "Nonpast"
+                    end
+                    verbs[hash["lemma"]][tense]["freq"]+=1
+                    
+                    #gathering info about the subject
+                    subjectinfo = check_for_subj_child(hash, sentence, lang)
                     nosubject = subjectinfo[0]
                     pronsubject = subjectinfo[1]
                     pron12subject = subjectinfo[2]
-                end
-            end
+                    if CHECK_COORD
+                        if nosubject and hash["rel"] == "conj" 
+                            hash2 = sentence[hash["head"]] #we are going up, to the first conjunct. In principle other conjuncts can be checked as well
+                            subjectinfo = check_for_subj_child(hash2, sentence, lang)
+                            nosubject = subjectinfo[0]
+                            pronsubject = subjectinfo[1]
+                            pron12subject = subjectinfo[2]
+                        end
+                    end
+                    expressed = 0                             
+                    pronexpressed = 0
+                    pron12expressed = 0
+                    
+                    #storing info about the subject
+                    if !nosubject
+                        expressed = 1
+                        verbs[hash["lemma"]][tense]["subj"] += 1
+                        if pronsubject
+                            pronexpressed = 1
+                            if pron12subject
+                                pron12expressed = 1
+                            end
+                        end
+                    end
             
-            expressed = 0                             
-            pronexpressed = 0
-            pron12expressed = 0
-            
-            if !nosubject
-                expressed = 1
-                verbs[hash["lemma"]][tense]["subj"] += 1
-                if pronsubject
-                    pronexpressed = 1
-                if pron12subject
-                    pron12expressed = 1
-                end
-                end
-            end
-            
-            if pron12expressed == 1
-                verbs[hash["lemma"]][tense]["expr12"] += 1
-            else    
-                verbs[hash["lemma"]][tense]["none"] += 1
-            end
-            
-            if ["rus","ukr","blg","bel"].include?(lang)
-                #printlemma = Translit.convert(hash["lemma"])
-                printlemma = hash["lemma"]
-            else
-                printlemma = hash["lemma"]
-            end
-            
-            clause_type = RELHASH[hash["rel"].split(":")[0]] #to cover subtypes like acl:relcl
-            if clause_type == "main"
-                if !check_for_subord_child(hash, sentence)
-                    clause_type = "simple"
-                end
-            end
+                    if pron12expressed == 1
+                        verbs[hash["lemma"]][tense]["expr12"] += 1
+                    else    
+                        verbs[hash["lemma"]][tense]["none"] += 1
+                    end
+                    
+                    #detecting clause type
+                    clause_type = RELHASH[hash["rel"].split(":")[0]] #to cover subtypes like acl:relcl
+                    if clause_type == "main"
+                        if !check_for_subord_child(hash, sentence)
+                            clause_type = "simple"
+                        end
+                    end
 
-            verbs_output[hash["lemma"]] << "#{printlemma}\t#{text}\t#{tense}\t#{findfeat(hash["feats"],"Aspect")}\t#{personhash[findfeat(hash["feats"],"Person")]}\t#{expressed}\t#{pronexpressed}\t#{pron12expressed}\t#{lang}\t#{group}\t#{hash["rel"]}\t#{clause_type}" #12 will ALWAYS be pronexpressed, while 3 won't. Remove this bias?
+                    printlemma = hash["lemma"] #Lemmas in Cyrillic used to be transliterated (to prevent issues when analyzing random-effects in R), but then I abandoned that
+                    verbs_output[hash["lemma"]] << "#{printlemma}\t#{text}\t#{tense}\t#{findfeat(hash["feats"],"Aspect")}\t#{personhash[findfeat(hash["feats"],"Person")]}\t#{expressed}\t#{pronexpressed}\t#{pron12expressed}\t#{lang}\t#{group}\t#{hash["rel"]}\t#{clause_type}" 
+                end
             end
-        end
-            sentence = Hash.new{|hash, key| hash[key] = Hash.new;} 
+            sentence = Hash.new{|hash, key| hash[key] = Hash.new;} #erasing the info about the old sentence
         end
     end
     
-    total_expr12_past = 0
-    total_expr12_nonpast = 0
-    total_none_past = 0
-    total_none_nonpast = 0
     
-    #o.puts "Verb\texpr12_past\texpr12_nonpast\ttotal_past\ttotal_nonpast"
-    verbs.each_pair do |verb, value|
-        if value["Past"]["freq"] > THRESHOLD_TOTAL and value["Nonpast"]["freq"] > THRESHOLD_TOTAL and (value["Nonpast"]["subj"] + value["Past"]["subj"]) > THRESHOLD_SUBJ 
-            
-            total_expr12_past += value["Past"]["expr12"]
-            total_expr12_nonpast += value["Nonpast"]["expr12"]
-            #total_none_past += value["Past"]["none"]
-            #total_none_nonpast += value["Nonpast"]["none"]
+    #output    
+    verbs.each_pair do |verb, value| #going through the information stored for all verbs
         
-            
-                #pastnone = value["Past"]["none"].to_f/value["Past"]["freq"].to_f 
-                #nonpastnone = value["Nonpast"]["none"].to_f/value["Nonpast"]["freq"].to_f 
-            pastexpr = value["Past"]["expr12"].to_f/value["Past"]["freq"].to_f 
-            nonpastexpr = value["Nonpast"]["expr12"].to_f/value["Nonpast"]["freq"].to_f 
-                
-            #o.puts "#{verb}\t#{pastexpr}\t#{nonpastexpr}\t#{value["Past"]["freq"]}\t#{value["Nonpast"]["freq"]}"
+        if value["Past"]["freq"] > THRESHOLD_TOTAL and value["Nonpast"]["freq"] > THRESHOLD_TOTAL and (value["Nonpast"]["subj"] + value["Past"]["subj"]) > THRESHOLD_SUBJ #and if the thresholds are passed...
             verbs_output[verb].each do |output|
-                #od.puts output
-                detfile.puts output
+                detfile.puts output #...output the info
             end
             
         end
     end
     
-    #o.puts "total_expr12_past\t#{total_expr12_past}"
-    #o.puts "total_expr12_nonpast\t#{total_expr12_nonpast}"
-    #o.puts "total_none_past\t#{total_none_past}"
-    #o.puts "total_none_nonpast\t#{total_none_nonpast}"
     
 end
